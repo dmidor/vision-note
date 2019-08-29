fe99680a9dda19b719d8164ea0944eca7a40ff22
Damian Midor <damian.midor@pragmaticcoders.com>
2019-08-29 18:56:33 +0200

    init service worker

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgOpenCVModule } from 'ng-open-cv';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+
+// const openCVConfig = {
+//   scriptUrl: 'assets/opencv/asm/3.4/opencv.js',
+//   usingWasm: false,
+//   locateFile: this.locateFile.bind(this),
+//   onRuntimeInitialized: () => {}
+// };
+//
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // NgOpenCVModule.forRoot(openCVConfig),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/camera.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { NgOpenCVService } from 'ng-open-cv';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CameraService {
+
+  constructor(private ngOpenCVService: NgOpenCVService) {
+  }
+
+
+  public capture(): void {
+
+  }
+
+}
